00d0ac295374cea9f6771f53c5dd3f8d5761506d
[01;31m[K"name"[m[K: "Felix Vandelanotte <[01;31m[K"email"[m[K: "vandelanottefelix@gmail.com>
2017-02-23 19:43:29 +0100

    initial greetings of bot

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  appId: '26414df0-37d6-4598-9fde-35ac4bd84f61',
+  appPassword: 'tajjacfFEDuvYHEJHGiM4pU',
+  recast: '01d66eacabae70f2af9ad9f7191a3704'
+}
+module.exports = config
---
+++ pearup.js
@@ -0,0 +1,37 @@
+const getGreetings = require('./intents/greetings.js')
+// const getInfoPokemon = require('./intents/infopokemon.js')
+const config = require('./config.js')
+const restify = require('restify')
+const builder = require('botbuilder')
+const recast = require('recastai')
+const recastClient = new recast.Client(config.recast)
+// Connection to Microsoft Bot Framework
+const connector = new builder.ChatConnector({
+  appId: config.appId,
+  appPassword: config.appPassword,
+})
+
+const bot = new builder.UniversalBot(connector)
+
+const INTENTS = {
+  greetings: getGreetings,
+  // infopokemon: getInfoPokemon,
+}
+
+// Event when Message received
+bot.dialog('/', (session) => {
+  recastClient.textRequest(session.message.text)
+  .then(res => {
+    const intent = res.intent()
+    const entity = res.get('pokemon')
+    if (intent) {
+      session.send(INTENTS[intent.slug](entity))
+    }
+  })
+  .catch(() => session.send('I need some sleep right now... Talk to me later!'))
+})
+
+// Server Init
+const server = restify.createServer()
+server.listen(8080)
+server.post('/', connector.listen())
